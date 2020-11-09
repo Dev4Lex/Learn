e00a393c89a88abd5540afd96f9e49827c70ec05
dev4lex <dev4lex@gmail.com>
2020-11-09 20:48:48 +0300

    update 09.11

---
+++ Selenium WebDriver + Java/src/LearnJava/LearnJava11.java
@@ -0,0 +1,24 @@
+package LearnJava;
+
+public class LearnJava11 {
+    public static void main(String[] args) {
+        //Исключения - ошибки, которые могут возникать при работе программы
+        try { //попытка выполнения
+            devide(10, 0);
+        } catch (ArithmeticException e) { //выполняется в случае ошибки
+            e.printStackTrace();
+        }
+        finally { //выполнятся в любом случае; опционально
+            System.out.println("");
+        }
+        }
+
+        public static void devide ( int a, int b) throws ArithmeticException {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot devide by zero!"); //исключение с сообщением об ошибке
+            } else {
+                System.out.println("Result is: " + a / b);
+
+            }
+        }
+    }
