33ff84382ea95eff5defa163dff999bb392ce4db
dev4lex <dev4lex@gmail.com>
2020-11-20 19:30:40 +0300

    20.11

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -7,6 +10,25 @@ public class MainClass {
 
         WebDriver driver = new FirefoxDriver(); //инициализация драйвера
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //неявное ожидание 10 секунд
+
+        driver.manage().window().maximize(); //окно на весь экран
+        //driver.manage().window().setSize(new Dimension(900,500)); //указание размеров экрана
+
+
         driver.get("https://yandex.ru/"); //переход по ссылке
+        driver.navigate().to("http://seleniumhq.org"); //альтернативный вариат перехода по ссылке
+        driver.navigate().back(); //возврат на предыдущую страницу
+        driver.navigate().forward(); //переход на страницу вперед
+        driver.navigate().refresh(); //обновление страницы
+
+
+        System.out.println(driver.getTitle()); //получение тайтла страницы
+        System.out.println(driver.getCurrentUrl()); //получение url страницы
+
+        driver.quit(); //завершение работы драйвера
+
+
+
     }
 }
---
+++ testselenium/src/main/java/MainClass1.java
@@ -0,0 +1,32 @@
+//Поиск элементов
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainClass1 {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "/Users/alexs/Desktop/git/Learn/testselenium/drivers/geckodriver"); //путь до драйвера
+
+        WebDriver driver = new FirefoxDriver(); //инициализация драйвера
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //неявное ожидание 10 секунд
+        driver.manage().window().maximize(); //окно на весь экран
+
+        driver.get("http://en.wikipedia.org");
+
+        WebElement link = driver.findElement(By.linkText("Log in")); //поиск по тексту ссылки
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate")); //поиск ссылки по части текста ссылки
+        WebElement searchField = driver.findElement(By.name("search")); //поиск по значению атрибута name
+        WebElement searchButton = driver.findElement(By.className("searchButton")); //поиск по имени класса
+        WebElement li = driver.findElement(By.id("ca-viewsource")); //поиск по значению id элкмента
+        WebElement input = driver.findElement(By.tagName("input")); //поиск по названию html-тега
+        WebElement element = driver.findElement(By.cssSelector("div#SimpleSearch input#searchButton")); // поиск по CSS-селектору
+        WebElement logo = driver.findElement(By.xpath("//div[@id='mw-panel']/div[@id='p-logo']//a")); //поиск по XPath
+
+        driver.quit();
+    }
+}
