9acef56c65354b89e6003cb4ad14544d412892ba
dev4lex <dev4lex@gmail.com>
2021-02-01 01:09:58 +0300

    upd 01.02

--- testselenium/src/main/java/MainClass3.java
+++ testselenium/src/main/java/MainClass3.java
@@ -18,19 +18,23 @@ public class MainClass3 {
         driver.manage().window().maximize(); //окно на весь экран
 
         driver.get("http://en.wikipedia.org");
-        driver.findElement(By.xpath("//input[@id='searchInput']")).sendKeys("Selenium WebDriver"); //вводит текста в поле
+        driver.findElement(By.xpath("//input[@id='searchInput']")).sendKeys("Selenium WebDriver"); //ввод текста в поле
         driver.findElement(By.xpath("//input[@id='searchButton']")).click();
         System.out.println(driver.findElement(By.xpath("//div[@id='searchText']/input")).getAttribute("value")); //получение значения атрибута
         driver.findElement(By.xpath("//div[@id='searchText']/input")).clear(); //очистка поля
 
-        /*driver.get("https://github.com");
-        WebElement button = driver.findElement(By.xpath("//button[@class = 'btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']"));
+        driver.get("https://github.com");
+        driver.findElement(By.xpath(".//*[@id='user[login]']")).sendKeys("testusername");
+        driver.findElement(By.xpath(".//*[@id='user[password]']")).sendKeys("testpass");WebElement button = driver.findElement(By.xpath("//button[@class = 'btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']"));
         button.submit(); //метод нажатия на кнопку
-        driver.findElement(By.xpath("//a[@class='HeaderMenu-link no-underline mr-3']")).click();
-
+//        driver.findElement(By.xpath("//a[@class='HeaderMenu-link no-underline mr-3']")).click();
+//
         driver.get("http://en-gb.facebook.com");
-        driver.findElement(By.xpath("//button[@id='u_0_b']")).submit();*/
+        driver.findElement(By.xpath("//*[@id='email']")).sendKeys("testusername");
+        driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("testpass");
+        driver.findElement(By.xpath("//*[@id='u_0_b']")).submit();
+        System.out.println("Mail is " + driver.findElement(By.xpath("//*[@id='email']")).getAttribute("value"));
 
-        //driver.quit();
+        driver.quit();
     }
 }
