f51dff842b90cbf2df94617caf4349312011484f
dev4lex <dev4lex@gmail.com>
2021-02-23 18:03:30 +0300

    Update MainClass.java

--- PageObject/src/main/java/MainClass.java
+++ PageObject/src/main/java/MainClass.java
@@ -1,6 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
+
+    static WebDriver driver;
 
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.chrome.driver", "/Users/alexs/Desktop/git/Learn/SeleniumWebDriver/drivers/chromedriver"); //путь до драйвера
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://github.com/");
+
+        MainPage mainPage = new MainPage(driver);
+
+        mainPage.register("test@test.com");
+
+        driver.quit();
     }
 }
