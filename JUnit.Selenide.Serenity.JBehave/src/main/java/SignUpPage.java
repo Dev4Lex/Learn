bbc50af4d0240f0209a81049c0ccf25afbc68941
dev4lex <dev4lex@gmail.com>
2021-03-26 18:21:02 +0300

    fix test typeInvalidYear

--- JUnit.Selenide.Serenity.JBehave/src/main/java/SignUpPage.java
+++ JUnit.Selenide.Serenity.JBehave/src/main/java/SignUpPage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -8,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 import static java.lang.String.format;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class SignUpPage {
     private WebDriver driver;
@@ -21,18 +23,18 @@ public class SignUpPage {
     private By passwordField = By.xpath("//input[@id='password']");
     private By nameField = By.xpath("//input[@id='displayname']");
     private By monthDropDown = By.xpath("//select[@id='month']");
-    private String monthDropDownOption = "//select[@id='month']/option[text()='$s']";
+    private String monthDropDownOption = "//option[text()='%s']";
     private By dayField = By.xpath("//input[@id='day']");
     private By yearField = By.xpath("//input[@id='year']");
-    private String genderRadioButton = "//div[@class='GenderSelect__FlexRow-v1a8zn-0 eHAjfo']/label/input[@value='$s']";
-    private By marketingCheckbox = By.xpath("//input[@name='marketing']");
+    private String genderRadioButton = "//div[@class='GenderSelect__FlexRow-v1a8zn-0 eHAjfo']/label/input[@value='%s']";
+    private By marketingCheckbox = By.xpath("//input[@name='marketing']/following-sibling::span[1]");
     private By thirdPartyCheckbox = By.xpath("//input[@name='thirdParty']");
     private By termsConditionsCheckbox = By.xpath("//*[@id=\"__next\"]/main/div[2]/form/div[9]/label/input");
     private By registerButton = By.xpath("//button[@type='submit']");
     private By emailErrorLabel = By.xpath("//div[contains(@class, 'InputErrorMessage')]/span[string-length(text())>0]");
     private String errorByEmail = "//div[contains(@class, 'InputErrorMessage')]/span[text()='Введите адрес электронной почты.']";
     private By errorLabel = By.xpath("//div[contains(@class, 'InputErrorMessage') and string-length(text()>0)]");
-    private String errorByText = "//div[contains(@class, 'InputErrorMessage') and text()='$s']";
+    private String errorByText = "//div[contains(@class, 'InputErrorMessage') and text()='%s']";
 
     public SignUpPage typeEmail(String email){
         driver.findElement(emailField).sendKeys(email);
@@ -56,7 +58,7 @@ public class SignUpPage {
 
     public SignUpPage setMonth(String month){
         driver.findElement(monthDropDown).click();
-        new WebDriverWait(driver,5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(format(monthDropDownOption,month)))).click();
+        new WebDriverWait(driver,30).until(visibilityOfElementLocated(By.xpath(String.format(monthDropDownOption, month)))).click();
         return this;
     }
 
@@ -123,5 +125,10 @@ public class SignUpPage {
     public boolean isEmailErrorVisible(String message){
         return driver.findElements(By.xpath(format(errorByEmail, message))).size()>0
                 && driver.findElements(By.xpath(format(errorByEmail, message))).get(0).isDisplayed();
+    }
+
+    public void getScroll(){
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0, window.innerHeight)", "");
     }
 }
--- JUnit.Selenide.Serenity.JBehave/src/test/java/SignUpTest.java
+++ JUnit.Selenide.Serenity.JBehave/src/test/java/SignUpTest.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,8 +11,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class SignUpTest {
-    WebDriver driver;
-    SignUpPage page;
+    private WebDriver driver;
+    private SignUpPage page;
 
     @Before
     public void setUp(){
@@ -22,18 +23,18 @@ public class SignUpTest {
         driver.get("https://www.spotify.com/ru-ru/signup/");
         driver.findElement(By.xpath("//button[contains(@class,\"mh-close\")]")).click();
         driver.findElement(By.xpath("//button[contains(@class,\"banner-close-button\")]")).click();
-
-
     }
 
 
     @Test
     public void typeInvalidYear(){
         page = new SignUpPage(driver);
-        page.setMonth("5")
+        page.getScroll();
+        page.setMonth("Май")
                 .typeDay("10")
-                .typeYear("85")
-                .setMarketing(true);
+                .typeYear("85");
+        page.getScroll();
+        page.setMarketing(true);
         Assert.assertTrue(page.isErrorVisible("Укажите действительный год."));
         Assert.assertFalse(page.isErrorVisible("Выберите месяц.")
                 && page.isErrorVisible("Укажите действительный день месяца."));
@@ -62,6 +63,8 @@ public class SignUpTest {
     @Test
     public void typeInvalidValues(){
         page = new SignUpPage(driver);
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0, window.innerHeight)", "");
         page.typeEmail("testmail")
                 .typeConfirmEmailField("wrong@test.mail")
                 .typePassword("qweqwe123!")
