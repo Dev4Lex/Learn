3027053f3b51b46e8bd7ce1efe4db4e006d474ea
dev4lex <dev4lex@gmail.com>
2021-03-26 16:09:49 +0300

    Create SignUpTest; add setup

---
+++ JUnit.Selenide.Serenity.JBehave/src/test/java/SignUpTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignUpTest {
+    WebDriver driver;
+    SignUpPage page;
+
+    @Before
+    public void setUp(){
+        System.setProperty();
+        System.setProperty("webdriver.chrome.driver", "/Users/alexs/Desktop/git/Learn/SeleniumWebDriver/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.spotify.com/ru-ru/signup/");
+    }
+}
