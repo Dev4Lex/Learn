94653239942875b14891fc5af239e28e21b4952e
dev4lex <dev4lex@gmail.com>
2021-03-30 23:15:48 +0300

    add packages pages/steps

--- JUnit.Selenide.Serenity.JBehave/src/test/java/SignUpTest.java
+++ JUnit.Selenide.Serenity.JBehave/src/test/java/SignUpTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.*;
+import pages.SignUpPage;
 
 
 import static com.codeborne.selenide.Condition.text;
